c823381a6e2ed98a5c3c1d194708b57944763dde
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-09-01 20:11:14 +0300

    create app styles

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -21,10 +21,7 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          Text(
-            "Best Seller",
-            style: Styles.titleMedium,
-          ),
+          Text("Best Seller", style: Styles.textStyle18),
           BestSellerListView(),
         ],
       ),
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logo = "assets/images/Logo.png";
   static const test_image = "assets/images/jungle_book.png";
+  static const kGFontFamily = "GT Sectra Fine";
 }
