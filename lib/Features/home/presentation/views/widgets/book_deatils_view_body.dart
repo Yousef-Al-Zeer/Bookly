85c0e31e12c201313ef3948998f75dfe7612cce4
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-09-04 23:10:45 +0300

    add custom book image to details view

--- lib/Features/home/presentation/views/widgets/book_deatils_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_deatils_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDeatilsViewBody extends StatelessWidget {
@@ -6,11 +7,16 @@ class BookDeatilsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           CustomBooksDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            child: CustomBookImage(),
+          ),
         ],
       ),
     );
