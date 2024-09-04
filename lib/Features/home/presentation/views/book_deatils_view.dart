8aa4c2faaa1d4fdaca864c47b63461cf0c85df16
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-09-04 19:38:26 +0300

    create books deatil view

---
+++ lib/Features/home/presentation/views/book_deatils_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDeatilsView extends StatelessWidget {
+  const BookDeatilsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/book_deatils_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDeatilsViewBody extends StatelessWidget {
+  const BookDeatilsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
