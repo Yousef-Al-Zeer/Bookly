174ae37b0c9ec50a0049527d451f4ddd82a2c0df
Yousef-Al-Zeer <iyousefzeer@gmail.com>
2024-09-05 14:53:32 +0300

    create similar books list view

--- lib/Features/home/presentation/views/widgets/book_deatils_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_deatils_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/Features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +18,11 @@ class BookDeatilsViewBody extends StatelessWidget {
         children: [
           CustomBooksDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.20),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.25),
             child: CustomBookImage(),
           ),
           const SizedBox(
-            height: 43,
+            height: 32,
           ),
           const Text(
             "The Jungle Book",
@@ -44,9 +45,28 @@ class BookDeatilsViewBody extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
           ),
           const SizedBox(
-            height: 32,
+            height: 24,
           ),
           const BooksAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "You may also like",
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksList(),
+          const SizedBox(
+            height: 40,
+          ),
         ],
       ),
     );
